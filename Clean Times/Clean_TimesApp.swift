ca3b6fb417123c090a48c9f60913b2fe7ab566c9
Pavel Arkharov <arkharovp@gmail.com>
2026-06-01 19:55:07 +0300

    Initial Commit

---
+++ Clean Times/Clean_TimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_TimesApp.swift
+//  Clean Times
+//
+//  Created by Pavel Arkharov on 1.6.2026.
+//
+
+import SwiftUI
+
+@main
+struct Clean_TimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clean TimesTests/Clean_TimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_TimesTests.swift
+//  Clean TimesTests
+//
+//  Created by Pavel Arkharov on 1.6.2026.
+//
+
+import Testing
+@testable import Clean_Times
+
+struct Clean_TimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
